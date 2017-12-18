f0c0bebb68f2c1b113c451b1933701639e0945db
Siwel Aragon <siwel2008@gmail.com>
2017-12-18 16:18:18 +1100

    d

--- QFSLINKS/Program.cs
+++ QFSLINKS/Program.cs
@@ -20,6 +20,6 @@ namespace QFSLINKS
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .Build();
+                .Build(); 
     }
 }
